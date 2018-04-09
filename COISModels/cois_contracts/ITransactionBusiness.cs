9f0604bfa47d1091af2fc9794bc8c5f5dc147328
sebasb93 <sebasb93@gmail.com>
2018-04-08 23:05:51 -0500

    #38: define contracts

---
+++ COISModels/cois_contracts/ITransactionBusiness.cs
@@ -0,0 +1,12 @@
+﻿using COIS_Models.transaction;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace COIS_Models.cois_contracts
+{
+    public interface ITransactionBusiness
+    {
+        List<Transaction> GetAll();
+    }
+}
---
+++ COISModels/transaction/Transaction.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace COIS_Models.transaction
+{
+    public class Transaction
+    {
+        public string Id { get; set; }
+    }
+}
