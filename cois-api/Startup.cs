8d65aad4a516616fc17ed84530a433d612a624eb
sebasb93 <sebasb93@gmail.com>
2018-04-08 23:49:27 -0500

    HotFix: Mongo Serialization

--- cois-api/Startup.cs
+++ cois-api/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using UtilsDB.contracts;
 
 namespace cois_api
 {
@@ -31,7 +32,7 @@ namespace cois_api
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IConventionRegistryService dbRegistryConvention)
         {
             if (env.IsDevelopment())
             {
@@ -39,6 +40,7 @@ namespace cois_api
             }
 
             app.UseMvc();
+            dbRegistryConvention.RegistryConvention();
         }
     }
 }
