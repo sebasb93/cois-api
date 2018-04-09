a4df79f861121347da46e2e8394ec717513f387b
sebasb93 <sebasb93@gmail.com>
2018-04-08 23:04:51 -0500

    #37: Define contracts

---
+++ COISModels/cois_contracts/IItemBusiness.cs
@@ -0,0 +1,13 @@
+﻿using COIS_Models.item;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace COIS_Models.cois_contracts
+{
+    public interface IItemBusiness
+    {
+        List<Item> GetAll();
+        string Test();
+    }
+}
---
+++ COISModels/item/Item.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace COIS_Models.item
+{
+    public class Item
+    {
+        public string Id { get; set; }
+    }
+}
